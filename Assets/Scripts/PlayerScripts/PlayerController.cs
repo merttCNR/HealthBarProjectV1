677e459bd7175bc1b6e513d771b8aa1bf5dcc53d
Mert Çınar <kadirmertcnr@gmail.com>
2023-04-17 06:14:56 +0300

    Player Movement Added.

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Inputs")]
+    float xInput;
+    [Header("Rigidbody2D")]
+    Rigidbody2D playerRigidbody;
+    [Header("Speed")]
+    float jumpSpeed = 30f;
+    float playerSpeed = 500f;
+    [Header("RaycastVar")]
+    private float raycastDistance = 1.5f;
+    public bool isGrounded;
+    public LayerMask groundLayer;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
+        InputChecker();
+        //GroundCheck();
+        PlayerJump();
+        PlayerMove();
     }
+    private void InputChecker(){
+        xInput = Input.GetAxis("Horizontal");
+    }
+    private void PlayerMove(){
+        playerRigidbody.velocity = new Vector2(xInput,0) * playerSpeed *Time.deltaTime;
+    }
+    private void PlayerJump(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            playerRigidbody.AddForce(Vector2.up * jumpSpeed,ForceMode2D.Impulse);
+        }
+    }
+   /*private void GroundCheck(){
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance , groundLayer);
+        Debug.DrawRay(transform.position,Vector2.down,Color.red);
+        if(hit.collider != null){
+            isGrounded = true;
+            Debug.Log(hit.collider.tag);
+        }
+       else{
+            isGrounded = false;
+        }
+    }*/
 }
--- Assets/Scripts/PlayerScripts/PlayerManager.cs
+++ Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    GameObject enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +12,9 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
         
     }
 }
